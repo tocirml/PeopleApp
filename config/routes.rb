7dc70c7896fbb7e6f0537bcc7d9b76e0e67b67c8
Roberto Munoz <tocirml@hotmail.com>
2017-01-18 21:45:41 -0600

    Add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  root "people#index"
+
+  root "pages#home"
+
+  get 'pages/about'
+
+  get 'pages/home'
+
+  get 'pages/contact'
+
   resources :people
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
